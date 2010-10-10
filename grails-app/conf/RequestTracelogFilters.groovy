61451dfd448862fd19e3e6c8a382686e243cd568
nobeans <nobeans@gmail.com>
2010-10-10 22:50:03 +0900

    changed to use debug log for logging instead of standard output

--- grails-app/conf/RequestTracelogFilters.groovy
+++ grails-app/conf/RequestTracelogFilters.groovy
@@ -7,59 +7,59 @@ class RequestTracelogFilters {
             all(controller:'*', action:'*') {
                 before = {
                     request.startedTime = System.currentTimeMillis()
-                    println "[Before Action]".padRight(60, ">")
-                    println "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    println "-"*20
-                    println "Request attributes:"
+                    log.debug "[Before Action]".padRight(60, ">")
+                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
+                    log.debug "-"*20
+                    log.debug "Request attributes:"
                     request.getAttributeNames().collect({it}).sort().each {
-                        println "  ${it} = ${request.getAttribute(it)}"
+                        log.debug "  ${it} = ${request.getAttribute(it)}"
                     }
-                    println "-"*20
-                    println "Request parameters:"
+                    log.debug "-"*20
+                    log.debug "Request parameters:"
                     params.keySet().sort().each {
-                        println "  ${it} = ${params[it]}"
+                        log.debug "  ${it} = ${params[it]}"
                     }
-                    println "-"*20
-                    println "Session attributes:"
+                    log.debug "-"*20
+                    log.debug "Session attributes:"
                     session.getAttributeNames().collect({it}).sort().each {
-                        println "  ${it} = ${session.getAttribute(it)}"
+                        log.debug "  ${it} = ${session.getAttribute(it)}"
                     }
-                    println "-"*20
-                    println "[Before Action]".padLeft(60, "<")
+                    log.debug "-"*20
+                    log.debug "[Before Action]".padLeft(60, "<")
                 }
                 after = {
-                    println "[After Action]".padRight(60, ">")
-                    println "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    println "-"*20
-                    println "Request parameters:"
+                    log.debug "[After Action]".padRight(60, ">")
+                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
+                    log.debug "-"*20
+                    log.debug "Request parameters:"
                     params.keySet().sort().each {
-                        println "  ${it} = ${params[it]}"
+                        log.debug "  ${it} = ${params[it]}"
                     }
-                    println "-"*20
-                    println "Session attributes:"
+                    log.debug "-"*20
+                    log.debug "Session attributes:"
                     session.getAttributeNames().collect({it}).sort().each {
-                        println "  ${it} = ${session.getAttribute(it)}"
+                        log.debug "  ${it} = ${session.getAttribute(it)}"
                     }
-                    println "-"*20
+                    log.debug "-"*20
                     def time = (System.currentTimeMillis() - request.startedTime) / 1000.0
-                    println "[After Action] (time: ${time}[sec])".padLeft(60, "<")
+                    log.debug "[After Action] (time: ${time}[sec])".padLeft(60, "<")
                 }
                 afterView = {
-                    println "[After View]".padRight(60, ">")
-                    println "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    println "-"*20
-                    println "Request parameters:"
+                    log.debug "[After View]".padRight(60, ">")
+                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
+                    log.debug "-"*20
+                    log.debug "Request parameters:"
                     params.keySet().sort().each {
-                        println "  ${it} = ${params[it]}"
+                        log.debug "  ${it} = ${params[it]}"
                     }
-                    println "-"*20
-                    println "Session attributes:"
+                    log.debug "-"*20
+                    log.debug "Session attributes:"
                     session.getAttributeNames().collect({it}).sort().each {
-                        println "  ${it} = ${session.getAttribute(it)}"
+                        log.debug "  ${it} = ${session.getAttribute(it)}"
                     }
-                    println "-"*20
+                    log.debug "-"*20
                     def time = (System.currentTimeMillis() - request.startedTime) / 1000.0
-                    println "[After View] (time: ${time}[sec])".padLeft(60, "<")
+                    log.debug "[After View] (time: ${time}[sec])".padLeft(60, "<")
                 }
             }
         }
