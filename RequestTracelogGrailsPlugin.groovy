c13525c65a7c851c92e77c2d98efc95f573476ea
Yasuharu NAKANO <ynak@jggug.org>
2012-05-31 19:08:46 +0900

    excluded unnecessary files

--- RequestTracelogGrailsPlugin.groovy
+++ RequestTracelogGrailsPlugin.groovy
@@ -7,7 +7,11 @@ class RequestTracelogGrailsPlugin {
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+        "grails-app/views/error.gsp",
+        "grails-app/domain/test/**",
+        "grails-app/i18n/**",
+        "web-app/**",
+        "scripts/**",
     ]
 
     // TODO Fill in these fields
