fc6d2fdb99270cb832ee37a3d282b5f04782fce0
Yasuharu NAKANO <ynak@jggug.org>
2012-05-31 16:23:01 +0900

    also output request attributes on [After Action] and [After View]

--- grails-app/conf/RequestTracelogFilters.groovy
+++ grails-app/conf/RequestTracelogFilters.groovy
@@ -8,61 +8,46 @@ class RequestTracelogFilters {
                 before = {
                     request.startedTime = System.currentTimeMillis()
                     log.debug "[Before Action]".padRight(60, ">")
-                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    log.debug "-"*20
-                    log.debug "Request attributes:"
-                    request.getAttributeNames().collect({it}).sort().each {
-                        log.debug "  ${it} = ${request.getAttribute(it)}"
-                    }
-                    log.debug "-"*20
-                    log.debug "Request parameters:"
-                    params.keySet().sort().each {
-                        log.debug "  ${it} = ${params[it]}"
-                    }
-                    log.debug "-"*20
-                    log.debug "Session attributes:"
-                    session.getAttributeNames().collect({it}).sort().each {
-                        log.debug "  ${it} = ${session.getAttribute(it)}"
-                    }
+                    traceLogOf(request, params, session)
                     log.debug "-"*20
                     log.debug "[Before Action]".padLeft(60, "<")
                 }
                 after = {
                     log.debug "[After Action]".padRight(60, ">")
-                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    log.debug "-"*20
-                    log.debug "Request parameters:"
-                    params.keySet().sort().each {
-                        log.debug "  ${it} = ${params[it]}"
-                    }
-                    log.debug "-"*20
-                    log.debug "Session attributes:"
-                    session.getAttributeNames().collect({it}).sort().each {
-                        log.debug "  ${it} = ${session.getAttribute(it)}"
-                    }
+                    traceLogOf(request, params, session)
                     log.debug "-"*20
                     def time = (System.currentTimeMillis() - request.startedTime) / 1000.0
                     log.debug "[After Action] (time: ${time}[sec])".padLeft(60, "<")
                 }
                 afterView = {
                     log.debug "[After View]".padRight(60, ">")
-                    log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
-                    log.debug "-"*20
-                    log.debug "Request parameters:"
-                    params.keySet().sort().each {
-                        log.debug "  ${it} = ${params[it]}"
-                    }
-                    log.debug "-"*20
-                    log.debug "Session attributes:"
-                    session.getAttributeNames().collect({it}).sort().each {
-                        log.debug "  ${it} = ${session.getAttribute(it)}"
-                    }
+                    traceLogOf(request, params, session)
                     log.debug "-"*20
                     def time = (System.currentTimeMillis() - request.startedTime) / 1000.0
                     log.debug "[After View] (time: ${time}[sec])".padLeft(60, "<")
                 }
             }
         }
+    }
+
+    private void traceLogOf(request, params, session) {
+        log.debug "Request ID: " + request["org.codehaus.groovy.grails.WEB_REQUEST"]
+        log.debug "-"*20
+        log.debug "Request attributes:"
+        request.getAttributeNames().collect({it}).sort().each {
+            log.debug "  ${it} = ${request.getAttribute(it)}"
+        }
+        log.debug "-"*20
+        log.debug "Request parameters:"
+        params.keySet().sort().each {
+            log.debug "  ${it} = ${params[it]}"
+        }
+        log.debug "-"*20
+        log.debug "Session attributes:"
+        session.getAttributeNames().collect({it}).sort().each {
+            log.debug "  ${it} = ${session.getAttribute(it)}"
+        }
+        log.debug "-"*20
     }
 }
 
