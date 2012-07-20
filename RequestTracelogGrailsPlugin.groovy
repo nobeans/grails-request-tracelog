a66b335293f74cbe28dff452edd2f367cc925e17
Yasuharu NAKANO <ynak@jggug.org>
2012-07-20 14:10:39 +0900

    bumped version to 0.2

--- RequestTracelogGrailsPlugin.groovy
+++ RequestTracelogGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RequestTracelogGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1.1 > *"
     // the other plugins this plugin depends on
@@ -13,8 +13,6 @@ class RequestTracelogGrailsPlugin {
         "web-app/**",
         "scripts/**",
     ]
-
-    // TODO Fill in these fields
     def author = "nobeans"
     def authorEmail = "nobeans@gmail.com"
     def title = "filter for debug logging"
@@ -24,34 +22,6 @@ It's useful for debugging.
 Logging is active only at development environment.
 It's very simple.
 '''
-
     // URL to the plugin's documentation
     def documentation = "http://grails.org/RequestTracelog+Plugin"
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
