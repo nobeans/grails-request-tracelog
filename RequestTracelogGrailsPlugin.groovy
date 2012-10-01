0ea49cd156ecf4b71a40ca1c0bf71ef8bc78111a
Yasuharu NAKANO <ynak@jggug.org>
2012-10-01 19:32:23 +0900

    added groupId

--- RequestTracelogGrailsPlugin.groovy
+++ RequestTracelogGrailsPlugin.groovy
@@ -1,4 +1,5 @@
 class RequestTracelogGrailsPlugin {
+    def groupId = 'org.jggug.kobo'
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
