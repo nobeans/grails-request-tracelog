2abb9804de6e89f4dfcb7062216b0ede5e0b5ebd
Yasuharu NAKANO <ynak@jggug.org>
2012-12-26 10:46:00 +0900

    init for 2.2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,40 +1,18 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.7
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target'
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+    inherits 'global'
+    log 'warn'
+
     repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
     }
 
     plugins {
-        build(
-            ":tomcat:$grailsVersion",
-            ":release:2.0.4",
-        ) {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
-        test(
-            ":hibernate:$grailsVersion",
-            ":spock:0.6",
-        ) {
-            export = false
-        }
-        //runtime ":svn:1.0.2"
     }
 }
