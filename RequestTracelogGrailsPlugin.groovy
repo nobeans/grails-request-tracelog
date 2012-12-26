1dfccb0f193ab8860d52492c9796275b1ad99c3b
Yasuharu NAKANO <ynak@jggug.org>
2012-12-26 10:26:20 +0900

    filled plugin descriptor out

--- RequestTracelogGrailsPlugin.groovy
+++ RequestTracelogGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class RequestTracelogGrailsPlugin {
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1.0 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -14,13 +14,17 @@ class RequestTracelogGrailsPlugin {
         "web-app/**",
         "scripts/**",
     ]
-    def author = "nobeans"
-    def authorEmail = "nobeans@gmail.com"
-    def title = "filter for debug logging"
+    def title = "Request Trace Log"
+    def author = "Yasuharu NAKANO"
+    def authorEmail = "ynak@jggug.org"
+    def license = "APACHE"
     def description = '''\\
 This plugin has a Filter to output some information of request/response to standard output.
 It's useful for debugging.
 Logging is active only at development environment.
 It's very simple.
 '''
+    def documentation = "http://github.com/nobeans/grails-request-tracelog/"
+    def scm = [url: "https://github.com/nobeans/grails-request-tracelog/"]
+    def issueManagement = [system: "GitHub Issues", url: "https://github.com/nobeans/grails-request-tracelog/issues"]
 }
