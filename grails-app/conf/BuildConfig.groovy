f748574bcc7f735c7c83d76e2fe9f4df3744ea93
Yasuharu Nakano <ynak@jggug.org>
2014-11-20 09:49:04 +0900

    Ready to publish to bintray

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,9 @@
 grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
 
+grails.project.repos.bintray.url = "https://api.bintray.com/maven/nobeans/maven/grails-request-tracelog"
+grails.project.repos.default = "bintray"
+
 grails.project.dependency.resolution = {
 
     inherits 'global'
@@ -8,10 +11,12 @@ grails.project.dependency.resolution = {
 
     repositories {
         grailsCentral()
+
+        mavenRepo "http://dl.bintray.com/upennlib/maven"
     }
 
     plugins {
-        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3', ':bintray-upload:0.2') {
             export = false
         }
     }
