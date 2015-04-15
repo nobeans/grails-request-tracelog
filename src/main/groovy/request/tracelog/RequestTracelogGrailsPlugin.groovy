f316248595258ab4d96543031aaf9ac2713c2d2c
Yasuharu Nakano <ynak@jggug.org>
2015-04-15 18:13:21 +0900

    Fix artifact name and group name

--- src/main/groovy/request/tracelog/RequestTracelogGrailsPlugin.groovy
+++ src/main/groovy/request/tracelog/RequestTracelogGrailsPlugin.groovy
@@ -4,11 +4,8 @@ import grails.plugins.*
 
 class RequestTracelogGrailsPlugin extends Plugin {
 
-    def groupId = 'org.jggug.kobo'
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "3.0.1 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp",
@@ -16,15 +13,13 @@ class RequestTracelogGrailsPlugin extends Plugin {
         "grails-app/i18n/**",
     ]
     def title = "Request Tracelog"
-    def author = "Yasuharu NAKANO"
+    def author = "Yasuharu Nakano"
     def authorEmail = "ynak@jggug.org"
     def license = "APACHE"
-    def description = '''\\
-This plugin has a Filter to output some information of request/response to standard output.
-It's useful for debugging.
-Logging is active only at development environment.
-It's very simple.
-'''
+    def description = '''\
+        |This plugin has a Interceptor to output some information of request/response to standard output.
+        |It's useful for debugging.  Logging is active only at development environment.  It's very simple.
+        |'''.stripMargin()
     def profiles = ['web']
     def documentation = "http://github.com/nobeans/grails-request-tracelog/"
     def scm = [url: "https://github.com/nobeans/grails-request-tracelog/"]
